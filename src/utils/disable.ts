62c6afaf00a4ef71e1e0e1fd837242db74ed3376
Kinga Johanna Szabó <kinga.johanna.szabo@zooshdigital.com>
2024-06-09 08:30:12 +0200

    fix

--- src/utils/disable.ts
+++ src/utils/disable.ts
@@ -1,3 +1,3 @@
 export const isVoteDisabled = () => {
-  return new Date().getTime() >= new Date("2024-06-09T07:00:00Z").getTime();
+  return new Date().getTime() >= new Date("2024-06-09T00:00:00Z").getTime();
 };
